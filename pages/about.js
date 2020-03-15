28894cab00136ae3b436c4317998d511eebb717c
Zé Yuri <jozeyuri@gmail.com>
2020-03-15 00:32:00 -0300

    navigation

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+export default function About() {
+  return (
+    <div><p>this is the about page</p></div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import Link from 'next/link'
+
+
 export default function Index() {
   return (
     <div>
+      <Link href="/about" title="About Page">
+        <a >About Page</a>
+      </Link>
       <p>Hello Next.js</p>
     </div>
   )
