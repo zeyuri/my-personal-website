464bcf88401078e5fa6dc6b3f417cca1b831d446
Zé Yuri <jozeyuri@gmail.com>
2020-03-15 16:54:56 -0300

    fetching data and getInitialProps lesson

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
 import Layout from "../components/MyLayout";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
+const Index = ({ shows }) => (
+  <Layout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {shows.map(({ id, name }) => (
+        <li key={id}>
+          <Link href="/p/[id]" as={`/p/${id}`}>
+            <a>{name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
 );
 
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="Hello Next.js" />
-        <PostLink id="Learn Next.js is awesome" />
-        <PostLink id="Deploy apps with Zeit" />
-      </ul>
-    </Layout>
-  );
-}
+Index.getInitialProps = async function() {
+  const res = await fetch("https://api.tvmaze.com/search/shows?q=batman");
+  const data = await res.json();
+
+  console.log(`Show Data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data.map(({ show }) => show)
+  };
+};
+
+export default Index;
